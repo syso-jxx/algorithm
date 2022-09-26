fc48413be2dd6b1e71e0a46cc57b4868d6aac12a
Yujin Cho <sysojxj@gmail.com>
2022-09-27 07:20:26 +0900

    백준 10809 알파벳 찾기

--- src/Baekjoon_10809.java
+++ src/Baekjoon_10809.java
@@ -2,21 +2,25 @@ import java.util.Scanner;
 
 public class Baekjoon_10809 {
     public static void main(String[] args) {
+/*
+        indexOf를 이용한 방법
 
-        Scanner put = new Scanner(System.in);
-        String str = put.next();
-        String abc = "abcdefghijklmnopqrstuvwxyz";
-        String result = "";
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        String[] arr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
-        for(int i = 0 ; i < str.length() ; i++) {
-            for(int j = 0 ; j < 26 ; j++) {
-                if(str.substring(i, i+1).equals(abc.substring(j, j+1))) {
-                    result = result+ " " + String.valueOf(i);
-                }else {
-                    result = result+ " " + "-1";
-                }
-            }
+        for (int i = 0; i < arr.length; i++){
+            int location = S.indexOf(arr[i]);
+            System.out.println(location);
         }
-        System.out.println(result);
+*/
+
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+
+        for (char c = 'a'; c <= 'z'; c++){
+            System.out.println(S.indexOf(c));
+        }
     }
 }
