5cdece2f098d109d178adf2e223bd725d382f304
Yujin Cho <sysojxj@gmail.com>
2022-09-29 23:44:11 +0900

    백준 15552 빠른 A+B

--- src/Baekjoon_15552.java
+++ src/Baekjoon_15552.java
@@ -3,22 +3,55 @@ import java.util.StringTokenizer;
 
 public class Baekjoon_15552 {
     public static void main(String[] args) throws IOException {
-        // 빠른 A+B
+/*
+//      BufferedWriter를 이용한 방법
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int T = Integer.parseInt(br.readLine()); // 테스트케이스의 개수 T를 입력
-
-        StringTokenizer st; // 입력받을 A와 B를 저장하기 위해 StringTokenizer 선언
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
         for(int i = 0; i < T; i++){
-            st = new StringTokenizer(br.readLine()); // 입력받을 A와 B를 입력
-            int A = Integer.parseInt(st.nextToken()); // A 저장
-            int B = Integer.parseInt(st.nextToken()); // B 저장
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
             bw.write(A + B + "\n");
         }
         bw.flush();
         bw.close();
         br.close();
+*/
+
+/*
+
+//      StringBuilder를 이용한 방법
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken()); // A 저장
+            int B = Integer.parseInt(st.nextToken()); // B 저장
+            sb.append(A + B).append('\n');
+        }
+        br.close();
+        System.out.println(sb);
+*/
+
+
+//      substring()를 이용한 방법
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            String str = br.readLine();
+            int blank = str.indexOf(" ");
+            int result = Integer.parseInt(str.substring(0, blank)) + Integer.parseInt(str.substring(blank + 1));
+            sb.append(result+"\n");
+        }
+        br.close();
+        System.out.print(sb);
     }
 }
